dba0651955206ffeba0806d16fa4efbf975c7710
Tanmay Desai <thinktanmay@gmail.com>
2020-03-07 16:29:25 +0530

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+const siteMetadata = {
+  title: `The Localhost Blog`,
+  description: `This is my coding blog where I write about my coding journey.`,
+  image: `/default-site-image.jpg`,
+  siteUrl: `https://the-localhost-blog.netlify.com`,
+  siteLanguage: `en-GB`,
+  siteLocale: `en_gb`,
+  twitterUsername: `@spences10`,
+  authorName: `Scott Spence`,
+}
+
+module.exports = {
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: { extensions: [`.mdx`, `.md`] },
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 540,
+          },
+        },
+      ],
+      plugin: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 540,
+          },
+        },
+      ],
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+}
